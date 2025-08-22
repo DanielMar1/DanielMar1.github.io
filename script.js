422a8bcaeb929fd33745b67d9dbe13206372be12
danielmargolin <margolin23+github1@gmail.com>
2025-08-22 10:27:45 +0300

    functionality

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 
 function changeColorToOrange() {
+    console.log('change to orange')
     document.body.style.backgroundColor = 'orange'
+    document.getElementById('change-color-btn').innerText = 'change to blue'
 }
 
-// document.getElementById('change-color-btn').addEventListener('click', changeColorToOrange)
-setTimeout(changeColorToOrange, 3000)
+function changeColorToBlue() {
+    console.log('change to blue')
+    document.body.style.backgroundColor = 'blue'
+    document.getElementById('change-color-btn').innerText = 'change to orange'
+}
+
+function changeColor() {
+    console.log('clicked on change color')
+    let buttonText = document.getElementById('change-color-btn').innerText // change to orange / change to blue
+    if (buttonText == 'change to orange') {
+        changeColorToOrange();
+    } else {
+        changeColorToBlue();
+    }
+}
+
+document.getElementById('change-color-btn').addEventListener('click', changeColor)
