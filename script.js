21bc5ba5572abc04dcaf23ff33ab5b14c1a1e302
danielmargolin <margolin23+github1@gmail.com>
2025-08-22 10:05:56 +0300

    js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function changeColorToOrange() {
+    document.body.style.backgroundColor = 'orange'
+}
+
+// document.getElementById('change-color-btn').addEventListener('click', changeColorToOrange)
+setTimeout(changeColorToOrange, 3000)
